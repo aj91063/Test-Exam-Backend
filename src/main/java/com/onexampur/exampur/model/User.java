cc09b3200460c355096eeeced6451ef17130bada
karan <abhishekjais1308@gmail.com>
2023-03-29 15:46:41 +0530

    authority enable

--- src/main/java/com/onexampur/exampur/model/User.java
+++ src/main/java/com/onexampur/exampur/model/User.java
@@ -145,7 +145,7 @@ public class User implements UserDetails {
         this.userRoles.forEach(userRole -> {
             authoritySet.add(new Authority(userRole.getRole().getRoleName()));
         });
-        return null;
+        return authoritySet;
     }
 
 
