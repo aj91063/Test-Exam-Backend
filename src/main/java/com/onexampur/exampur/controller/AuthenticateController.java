6aac6833a1eedbe066ae32edb1477f932372dfd1
karan <abhishekjais1308@gmail.com>
2023-04-01 17:43:02 +0530

    adding crossorigin annotation

--- src/main/java/com/onexampur/exampur/controller/AuthenticateController.java
+++ src/main/java/com/onexampur/exampur/controller/AuthenticateController.java
@@ -3,6 +3,7 @@ package com.onexampur.exampur.controller;
 import com.onexampur.exampur.config.JwtUtils;
 import com.onexampur.exampur.model.JwtRequest;
 import com.onexampur.exampur.model.JwtResponce;
+import com.onexampur.exampur.model.User;
 import com.onexampur.exampur.servises.serviceImpl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +13,12 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
 public class AuthenticateController {
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -27,11 +29,11 @@ public class AuthenticateController {
 
 @PostMapping("/generate-token")
     public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtRequest) throws Exception {
-            try{
-                this.authenticate(jwtRequest.getUserName(),jwtRequest.getPassword());
-            } catch ( Exception e) {
-                throw new Exception("User not found!! "+e.getMessage());
-            }
+        try {
+            this.authenticate(jwtRequest.getUserName(), jwtRequest.getPassword());
+        } catch (Exception e) {
+            throw new Exception("User not found!! " + e.getMessage());
+        }
 
         final UserDetails userDetails = userDetailsService.loadUserByUsername(jwtRequest.getUserName());
         final String token = this.jwtUtils.generateToken(userDetails);
@@ -39,13 +41,18 @@ public class AuthenticateController {
     }
 
 
+    @GetMapping("/currentUser")
+    public User getCurrentLoginUser(Principal principal) {
+        return (User) this.userDetailsService.loadUserByUsername(principal.getName());
+    }
+
     private void authenticate(String userName, String password) throws Exception {
-                try{
-                        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName,password));
-                }catch(DisabledException e){
-                    throw new Exception("USER DISABLED "+e.getMessage());
-                }catch (BadCredentialsException  e){
-                    throw new Exception("Invalid Credential "+e.getMessage());
-                }
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER DISABLED " + e.getMessage());
+        } catch (BadCredentialsException e) {
+            throw new Exception("Invalid Credential " + e.getMessage());
+        }
     }
 }
