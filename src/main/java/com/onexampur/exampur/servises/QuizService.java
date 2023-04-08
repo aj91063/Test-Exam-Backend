818a459091cea6e405f45f891100f4ac12206355
karan <abhishekjais1308@gmail.com>
2023-04-08 12:48:40 +0530

    QuizServiceImpl.java done

--- src/main/java/com/onexampur/exampur/servises/QuizService.java
+++ src/main/java/com/onexampur/exampur/servises/QuizService.java
@@ -1,4 +1,18 @@
 package com.onexampur.exampur.servises;
 
+import com.onexampur.exampur.model.Quize.Quize;
+
+import java.util.Set;
+
 public interface QuizService {
+
+    Quize addQuize(Quize quize);
+
+    Quize updateQuiz(Quize quize);
+
+   Set<Quize> getQuizzes();
+
+   Quize getQuiz(Long quizId);
+
+   void deleteQuiz(Long quizId);
 }
---
+++ src/main/java/com/onexampur/exampur/servises/serviceImpl/QuizServiceImpl.java
@@ -0,0 +1,42 @@
+package com.onexampur.exampur.servises.serviceImpl;
+
+import com.onexampur.exampur.model.Quize.Quize;
+import com.onexampur.exampur.repository.QuizRepository;
+import com.onexampur.exampur.servises.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class QuizServiceImpl implements QuizService {
+
+    @Autowired
+    QuizRepository quizRepository;
+    @Override
+    public Quize addQuize(Quize quize) {
+
+        return  this.quizRepository.save(quize);
+    }
+
+    @Override
+    public Quize updateQuiz(Quize quize) {
+        return  this.quizRepository.save(quize);
+    }
+
+    @Override
+    public Set<Quize> getQuizzes() {
+        return new LinkedHashSet<>(this.quizRepository.findAll());
+    }
+
+    @Override
+    public Quize getQuiz(Long quizId) {
+        return this.quizRepository.findById(quizId).get();
+    }
+
+    @Override
+    public void deleteQuiz(Long quizId) {
+        Quize quize = new Quize();
+        quize.setqId(quizId);
+        this.quizRepository.delete(quize);
+    }
+}
